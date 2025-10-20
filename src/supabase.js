23ae02bbd8ad962170238a5d89b911ce4a00a619
Muhammad Nashit <muhammadnashitulislam@gmail.com>
2025-10-20 11:19:55 +0500

    Add Supabase client initialization

---
+++ src/supabase.js
@@ -0,0 +1,7 @@
+// /js/supabase.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+  "https://ouuylqwsahvwaoxipjum.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91dXlscXdzYWh2d2FveGlwanVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5MDU5ODYsImV4cCI6MjA3NjQ4MTk4Nn0.XkA9_WH4vQ3AM_7jOdJOxjrZwE_A9XAq42Gn0F3z14Y"
+);
