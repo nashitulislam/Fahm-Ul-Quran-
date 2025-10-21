ae60c8895ab4f9b33a82b0dd8c6188b0cbc1711e
Muhammad Nashit <muhammadnashitulislam@gmail.com>
2025-10-21 11:05:11 +0500

    Initialize Supabase client with URL and anon key
    Updated Supabase client initialization with project URL and anon key.

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
-// src/supabase.js
 import { createClient } from "@supabase/supabase-js";
 
+// Supabase project URL aur anon key
 export const supabase = createClient(
   "https://geognghitesjcsoiqldz.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdlb2duZ2hpdGVzamNzb2lxbGR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5Mzk4NDMsImV4cCI6MjA3NjUxNTg0M30.htqWoXWk-jO00Wlgc-mFu7ZtbtEj40025MTjGidbues"
