6b0b025a7ecf4e319f81c5e779ce7924759f4326
Muhammad Nashit <muhammadnashitulislam@gmail.com>
2025-10-21 10:54:29 +0500

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -2,6 +2,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://ouuylqwsahvwaoxipjum.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91dXlscXdzYWh2d2FveGlwanVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5MDU5ODYsImV4cCI6MjA3NjQ4MTk4Nn0.XkA9_WH4vQ3AM_7jOdJOxjrZwE_A9XAq42Gn0F3z14Y"
+  "https://geognghitesjcsoiqldz.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdlb2duZ2hpdGVzamNzb2lxbGR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5Mzk4NDMsImV4cCI6MjA3NjUxNTg0M30.htqWoXWk-jO00Wlgc-mFu7ZtbtEj40025MTjGidbues"
 );
